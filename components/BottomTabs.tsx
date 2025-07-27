522a0628bbd4807dd7d2b634b5b4c0ad922a889b
Attackeler <teocostin23@yahoo.com>
2025-07-27 15:56:03 +0300

    Added more buttons

--- components/BottomTabs.tsx
+++ components/BottomTabs.tsx
@@ -8,40 +8,40 @@ import HomeScreen from '../screens/HomeScreen';
 const Tab = createBottomTabNavigator();
 
 export default function ButtonTabs() {
-  const theme = useTheme();
+    const theme = useTheme();
 
-  return (
-    <Tab.Navigator
-      initialRouteName="Home"
-      screenOptions={({ route }) => ({
-        headerShown: false,
-        tabBarShowLabel: false,
-        tabBarActiveTintColor: theme.colors.primary,
-        tabBarInactiveTintColor: theme.colors.outline,
-        tabBarStyle: {
-          backgroundColor: theme.colors.surface, // optional!
-        },
-        tabBarIcon: ({ color, size }) => {
-          let iconName: keyof typeof MaterialCommunityIcons.glyphMap;
-          switch (route.name) {
-            case 'Home':
-              iconName = 'home';
-              break;
-            default:
-              iconName = 'help-circle';
-          }
-          return (
-            <MaterialCommunityIcons name={iconName} size={size} color={color} />
-          );
-        },
-      })}
-    >
-<Tab.Screen name="Home" component={HomeScreen} />
-<Tab.Screen name="Games" component={HomeScreen} />
-<Tab.Screen name="Friends" component={HomeScreen} />
-<Tab.Screen name="Profile" component={HomeScreen} />
-<Tab.Screen name="Stats" component={HomeScreen} />
+    return (
+        <Tab.Navigator
+            initialRouteName="Home"
+            screenOptions={({ route }) => ({
+                headerShown: false,
+                tabBarShowLabel: false,
+                tabBarActiveTintColor: theme.colors.primary,
+                tabBarInactiveTintColor: theme.colors.outline,
+                tabBarStyle: {
+                    backgroundColor: theme.colors.surface, // optional!
+                },
+                tabBarIcon: ({ color, size }) => {
+                    let iconName: keyof typeof MaterialCommunityIcons.glyphMap;
+                    switch (route.name) {
+                        case 'Home':
+                            iconName = 'home';
+                            break;
+                        default:
+                            iconName = 'help-circle';
+                    }
+                    return (
+                        <MaterialCommunityIcons name={iconName} size={size} color={color} />
+                    );
+                },
+            })}
+        >
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Games" component={HomeScreen} />
+            <Tab.Screen name="Friends" component={HomeScreen} />
+            <Tab.Screen name="Profile" component={HomeScreen} />
+            <Tab.Screen name="Stats" component={HomeScreen} />
 
-    </Tab.Navigator>
-  );
+        </Tab.Navigator>
+    );
 }
